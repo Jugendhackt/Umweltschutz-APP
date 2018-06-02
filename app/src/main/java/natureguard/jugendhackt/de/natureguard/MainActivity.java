79ed93aebeab84c042314b9afdc7f737abde9573
Riko Gercke <riko.gercke@gmail.com>
2018-06-02 21:39:59 +0200

    added json get

--- app/src/main/java/natureguard/jugendhackt/de/natureguard/MainActivity.java
+++ app/src/main/java/natureguard/jugendhackt/de/natureguard/MainActivity.java
@@ -1,13 +1,69 @@
 package natureguard.jugendhackt.de.natureguard;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
 public class MainActivity extends AppCompatActivity {
+    public static String dataURL = "http://192.168.42.224/getdata.php";
+    String data ="";
+    String dataParsed = "";
+    String singleParsed ="";
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            URL url = new URL("http://192.168.42.224/getdata.php");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while (line != null) {
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            JSONArray JA = new JSONArray(data);
+            for (int i = 0; i < JA.length(); i++) {
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed = "user_ID:" + JO.get("user_ID") + "\n" +
+                        "user_name:" + JO.get("user_password") + "\n" +
+                        "user_password:" + JO.get("user_password") + "\n" +
+                        "Contact:" + JO.get("contact") + "\n" +
+                        "Country:" + JO.get("country") + "\n";
+
+                dataParsed = dataParsed + singleParsed + "\n";
+
+
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 }
